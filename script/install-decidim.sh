1aafb876d2d9b5d5b969facfb0603950c1097cb6
Ivan Vergés <ivan@platoniq.net>
2020-12-22 13:01:23 +0100

    fix ubuntu name

--- script/install-decidim.sh
+++ script/install-decidim.sh
@@ -543,7 +543,7 @@ step_servers(){
 		sudo apt-get install -y dirmngr gnupg
 		sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
 		sudo apt-get install -y apt-transport-https ca-certificates
-		sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger bionic main > /etc/apt/sources.list.d/passenger.list'
+		sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger $(lsb_release -cs) main > /etc/apt/sources.list.d/passenger.list'
 		sudo apt-get update
 	fi
 
